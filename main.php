1268791a3e2792296029ff5808dc47360d14b665
ys <y.syoji@kura-corpo.co.jp>
2018-02-07 11:29:39 +0000

    2

---
+++ main.php
@@ -0,0 +1,22 @@
+<?php
+
+$html="";
+$html.="<html>";
+$html.="<head>";
+$html.="<meta http-equiv='Refresh' content='1000'>";
+$html.="<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>";
+$html.="<script src='//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>";
+$html.="</head>";
+$html.="<script type='text/javascript'>";
+
+$html.="<body>";
+$html.="<h1>test</h1>";
+
+
+
+$html.="</body>";
+
+$html.="</html>";
+print $html;
+
+?>
