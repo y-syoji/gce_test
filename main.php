d99414541811732092d7d8617a4638926cfbc906
ys <y.syoji@kura-corpo.co.jp>
2018-02-07 11:59:58 +0000

    3

--- main.php
+++ main.php
@@ -1,7 +1,26 @@
 <?php
 
-$html="";
+$html="start";
 $html.="<html>";
+$html.="<head>";
+$html.="<meta http-equiv='Refresh' content='1000'>";
+$html.="<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>";
+$html.="<script src='//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>";
+$html.="</head>";
+
+
+$html.="
+<script type='text/javascript'>
+$(document).ready(function(){
+alert('jQueryファイルの読み込み完了でーす。');
+});
+</script>";
+
+
+$html.="<h1>test</h1>";
+$html.="<br>test";
+$html.="</html>";
+/*$html.="<html>";
 $html.="<head>";
 $html.="<meta http-equiv='Refresh' content='1000'>";
 $html.="<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>";
@@ -10,13 +29,10 @@ $html.="</head>";
 $html.="<script type='text/javascript'>";
 
 $html.="<body>";
+$html.="test";
 $html.="<h1>test</h1>";
-
-
-
 $html.="</body>";
-
-$html.="</html>";
+$html.="</html>";*/
 print $html;
 
 ?>
