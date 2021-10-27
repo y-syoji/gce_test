f67ecd81118e40e34fdad2f187692192a8ff56b5
SYOJI <y-syoji@kura-corpo.co.jp>
2021-10-27 03:56:42 +0000

    tmp

--- main.php
+++ main.php
@@ -8,7 +8,6 @@ $html.="<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>";
 $html.="<script src='//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>";
 $html.="</head>";
 
-
 $html.="
 <script type='text/javascript'>
 $(document).ready(function(){
@@ -16,23 +15,7 @@ alert('jQueryファイルの読み込み完了でーす。');
 });
 </script>";
 
-
 $html.="<h1>test</h1>";
 $html.="<br>test";
 $html.="</html>";
-/*$html.="<html>";
-$html.="<head>";
-$html.="<meta http-equiv='Refresh' content='1000'>";
-$html.="<meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>";
-$html.="<script src='//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js'></script>";
-$html.="</head>";
-$html.="<script type='text/javascript'>";
-
-$html.="<body>";
-$html.="test";
-$html.="<h1>test</h1>";
-$html.="</body>";
-$html.="</html>";*/
-print $html;
-
 ?>
